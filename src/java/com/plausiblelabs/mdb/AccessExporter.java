6b0fd60bac25b1568ae919cd85ebbd16c038878f
Ronny Haryanto <ronny@haryan.to>
2014-11-03 16:30:06 +1100

    Spaces in identifiers are replaced with underscores

--- src/java/com/plausiblelabs/mdb/AccessExporter.java
+++ src/java/com/plausiblelabs/mdb/AccessExporter.java
@@ -13,7 +13,7 @@
  * 3. Neither the name of the copyright holder nor the names of any contributors
  *    may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -52,23 +52,23 @@ public class AccessExporter {
     /**
      * Create a new exporter with the provided MS Access
      * database
-     * 
+     *
      * @param db A reference to an Access database.
      */
     public AccessExporter (Database db) {
         this.db = db;
     }
-    
+
     /* XXX: Manual escaping of identifiers. */
     private String escapeIdentifier (final String identifier) {
-        return "'" + identifier.replace("'", "''") + "'";
+        return "'" + identifier.replace("'", "''").replace(" ", "_") + "'";
     }
 
 
     /**
      * Iterate over and create SQLite indeces for every index defined
      * in the MS Access table.
-     * 
+     *
      * @param table MS Access table
      * @param jdbc The SQLite database JDBC connection
      * @throws SQLException
@@ -83,7 +83,7 @@ public class AccessExporter {
 
     /**
      * Create an index in an SQLite table for the corresponding index in MS Access
-     * 
+     *
      * @param table MS Access table
      * @param jdbc The SQLite database JDBC connection
      * @throws SQLException
@@ -91,7 +91,7 @@ public class AccessExporter {
     private void createIndex(final Index index, final Connection jdbc) throws SQLException {
         final List<Index.ColumnDescriptor> columns = index.getColumns();
         final StringBuilder stmtBuilder = new StringBuilder();
-        
+
         /* Create the statement */
         final String tableName = index.getTable().getName();
         final String indexName = tableName + "_" + index.getName();
@@ -118,10 +118,10 @@ public class AccessExporter {
 
     /**
      * Create an SQLite table for the corresponding MS Access table.
-     * 
+     *
      * @param table MS Access table
      * @param jdbc The SQLite database JDBC connection
-     * @throws SQLException 
+     * @throws SQLException
      */
     private void createTable (final Table table, final Connection jdbc) throws SQLException {
         final List<Column> columns = table.getColumns();
@@ -129,11 +129,11 @@ public class AccessExporter {
 
         /* Create the statement */
         stmtBuilder.append("CREATE TABLE " + escapeIdentifier(table.getName()) + " (");
-        
+
         final int columnCount = columns.size();
         for (int i = 0; i < columnCount; i++) {
             final Column column = columns.get(i);
-            
+
             stmtBuilder.append(escapeIdentifier(column.getName()));
             stmtBuilder.append(" ");
             switch (column.getType()) {
@@ -142,7 +142,7 @@ public class AccessExporter {
                 case OLE:
                     stmtBuilder.append("BLOB");
                     break;
-                
+
                 /* Integers */
                 case BOOLEAN:
                 case BYTE:
@@ -150,19 +150,19 @@ public class AccessExporter {
                 case LONG:
                     stmtBuilder.append("INTEGER");
                     break;
-              
+
                 /* Timestamp */
                 case SHORT_DATE_TIME:
                     stmtBuilder.append("DATETIME");
                     break;
-               
+
                 /* Floating point */
                 case DOUBLE:
                 case FLOAT:
                 case NUMERIC:
                     stmtBuilder.append("DOUBLE");
                     break;
-                
+
                 /* Strings */
                 case TEXT:
                 case GUID:
@@ -178,12 +178,12 @@ public class AccessExporter {
                 default:
                     throw new SQLException("Unhandled MS Acess datatype: " + column.getType());
             }
-            
+
             if (i + 1 < columnCount)
                 stmtBuilder.append(", ");
         }
         stmtBuilder.append(")");
-        
+
         /* Execute it */
         final Statement stmt = jdbc.createStatement();
         stmt.execute(stmtBuilder.toString());
@@ -192,12 +192,12 @@ public class AccessExporter {
     /**
      * Iterate over and create SQLite tables for every table defined
      * in the MS Access database.
-     * 
+     *
      * @param jdbc The SQLite database JDBC connection
      */
     private void createTables (final Connection jdbc) throws IOException, SQLException {
         final Set<String> tableNames = db.getTableNames();
-        
+
         for (String tableName : tableNames) {
             Table table = db.getTable(tableName);
             createTable(table, jdbc);
@@ -205,47 +205,47 @@ public class AccessExporter {
         }
     }
 
-    
+
     private void populateTable (Table table, Connection jdbc) throws SQLException, IOException {
         final List<Column> columns = table.getColumns();
         final StringBuilder stmtBuilder = new StringBuilder();
         final StringBuilder valueStmtBuilder = new StringBuilder();
-        
+
         /* Record the column count */
         final int columnCount = columns.size();
-        
+
         /* Build the INSERT statement (in two pieces simultaneously) */
         stmtBuilder.append("INSERT INTO " + escapeIdentifier(table.getName()) + " (");
         valueStmtBuilder.append("(");
-        
+
         for (int i = 0; i < columnCount; i++) {
             final Column column = columns.get(i);
 
             /* The column name and the VALUE binding */
             stmtBuilder.append(escapeIdentifier(column.getName()));
             valueStmtBuilder.append("?");
-            
+
             if (i + 1 < columnCount) {
                 stmtBuilder.append(", ");
                 valueStmtBuilder.append(", ");
             }
         }
-        
+
         /* Now append the VALUES piece */
         stmtBuilder.append(") VALUES ");
         stmtBuilder.append(valueStmtBuilder);
         stmtBuilder.append(")");
-        
+
         /* Create the prepared statement */
         final PreparedStatement prep = jdbc.prepareStatement(stmtBuilder.toString());
-        
+
         /* Kick off the insert spree */
         for (Map<String, Object> row : table) {
             /* Bind all the column values. We let JDBC do type conversion -- is this correct?. */
             for (int i = 0; i < columnCount; i++) {
                 final Column column = columns.get(i);
                 final Object value = row.get(column.getName());
-                
+
                 /* If null, just bail out early and avoid a lot of NULL checking */
                 if (value == null) {
                     prep.setObject(i + 1, value);
@@ -279,7 +279,7 @@ public class AccessExporter {
                             intVal = 1;
                         else
                             intVal = 0;
-                            
+
                         /* Store it */
                         prep.setInt(i + 1, intVal);
                         break;
@@ -287,46 +287,46 @@ public class AccessExporter {
                         prep.setObject(i + 1, row.get(column.getName()));
                         break;
                 }
-                
+
             }
-            
+
             /* Execute the insert */
             prep.executeUpdate();
         }
     }
-    
+
     /**
      * Iterate over all data and populate the SQLite tables
      * @param jdbc The SQLite database JDBC connection
-     * @throws IOException 
-     * @throws SQLException 
+     * @throws IOException
+     * @throws SQLException
      */
     private void populateTables (final Connection jdbc) throws IOException, SQLException {
         final Set<String> tableNames = db.getTableNames();
-        
+
         for (String tableName : tableNames) {
             Table table = db.getTable(tableName);
             populateTable(table, jdbc);
         }
     }
-    
+
     /**
      * Export the Access database to the given SQLite JDBC connection.
      * The referenced SQLite database should be empty.
-     * 
+     *
      * @param jdbc A JDBC connection to a SQLite database.
-     * @throws SQLException 
+     * @throws SQLException
      */
     public void export (final Connection jdbc) throws IOException, SQLException {
         /* Start a transaction */
         jdbc.setAutoCommit(false);
-        
+
         /* Create the tables */
         createTables(jdbc);
-        
+
         /* Populate the tables */
         populateTables(jdbc);
-        
+
         jdbc.commit();
         jdbc.setAutoCommit(true);
     }
